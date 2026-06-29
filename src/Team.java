0c2dde90acd347d502f84567d083eb899f3609fa
sumal829 <samirpanjiyar4@gmail.com>
2026-06-29 14:45:13 +0545

    added has a and is a relationship

--- src/Team.java
+++ src/Team.java
@@ -1,20 +1,54 @@
 public class Team {
     Member member;
+    Captain captain;
 
-    public Team(Member member) {
+    public Team(Member member,Captain captain) {
         this.member = member;
+        this.captain=captain;
     }
+
+
 
     public static void main(String[] args) {
         Member myMember = new Member("samir", "panjiyar", 10, 1);
-        Team myTeam = new Team(myMember);
+        Captain myCaptain=new Captain("Anupa",19,23,"gurung","silver",3);
+        Team myTeam = new Team(myMember,myCaptain);
+
         System.out.println(myTeam.member.getName());
         System.out.println(myTeam.member.getLastname());
         System.out.println(myTeam.member.getAge());
         System.out.println(myTeam.member.getRoll());
+
+
+
+        myCaptain.setName("Ravi");
+        System.out.println(myCaptain.getName());
+        System.out.println(myCaptain.getLastname());
+        System.out.println(myTeam.captain.getName());
+        System.out.println(myTeam.captain.getRank());
+        System.out.println(myTeam.captain.getScore());
     }
 }
 
+    class Captain extends Member{
+
+        private String rank;
+        private int score;
+
+        public Captain(String name,int age,int roll,String lastname,String rank,int score){
+            super(name, lastname, age, roll);
+            this.rank=rank;
+            this.score=score;
+        }
+
+        public String getRank(){
+            return this.rank;
+
+        }
+        public int getScore(){
+            return this.score;
+        }
+    }
     class Member{
         private String name;
         private String lastname;
@@ -27,9 +61,16 @@ public class Team {
             this.age=age;
             this.roll=roll;
         }
-        public String getName(){
-            return this.name;
+        public void setName(String name) {
+            if(name != null && name.length() >= 2){
+                this.name = name;
+            }
         }
+
+        public String getName() {
+            return name;
+        }
+
 
         public String getLastname(){
             return this.lastname;
