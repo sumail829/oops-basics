307ca60293a2fe9d8ab9e19faf23f7d85977b7d3
sumal829 <samirpanjiyar4@gmail.com>
2026-07-24 22:48:56 +0545

    reversed string

---
+++ src/ReverseString.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+
+public class ReverseString{
+    public static void main(String[] args){
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter the string to reverse");
+        String s=scanner.nextLine();
+        System.out.println(s);
+        char[] rev=s.toCharArray();
+        System.out.println(rev);
+        for(int i=rev.length-1;i>=0;i--){
+            System.out.print(rev[i]);
+        }
+    }
+}
